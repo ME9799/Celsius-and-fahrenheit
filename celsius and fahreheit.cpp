4885eb6555400c65a820f74806d16713704b0004
ME9799 <93213577+ME9799@users.noreply.github.com>
2021-11-09 18:49:20 +0300

    Add files via upload

---
+++ celsius and fahreheit.cpp
@@ -0,0 +1,19 @@
+// C++ program to perform celsius to fahrenheit conversion
+ 
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float  Fahrenheit, Celsius;
+	cout<<"Enter tempreture in Fahrenheit"<<endl;
+	cin>> Fahrenheit;
+	
+	Celsius = (5.0/9.0) * (Fahrenheit – 32.0)<<endl;
+	cout<<"Temperature in celsius: "<<celsius<<endl;
+	cout<<"Temperature in fahrenheit: "<<fahrenheit<<endl;
+	
+	return 0;
+}
